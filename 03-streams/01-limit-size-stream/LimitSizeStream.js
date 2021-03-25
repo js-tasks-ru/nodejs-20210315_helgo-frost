7f3a5bd43b220c129dcad2bb43c70226f2ee4fa8
Oleg Ledvanov <olegledvanov@gmail.com>
2021-03-25 15:32:53 +0300

    complete HW #3 task 1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,18 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this._totalBytesTransfered = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this._totalBytesTransfered += chunk.length;
+    this._totalBytesTransfered > this.limit
+      ? callback(new LimitExceededError())
+      : callback(null, chunk);
   }
 }
 
