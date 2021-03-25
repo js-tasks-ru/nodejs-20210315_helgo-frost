fc0a39505caae81a542a3feaeb6a96b4f6592e0e
Oleg Ledvanov <olegledvanov@gmail.com>
2021-03-25 20:29:29 +0300

    do hw3 in progress

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,28 @@
-const stream = require('stream');
-const os = require('os');
+const stream = require("stream");
+const os = require("os");
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.bufer = "";
   }
 
   _transform(chunk, encoding, callback) {
+    let splited = chunk.toString().split(os.EOL);
+
+    splited.forEach((p, index, arr) => {
+      if (index == arr.length - 1) {
+        this.bufer = p.toString();
+        return;
+      }
+      this.push(p.toString());
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.bufer);
   }
 }
 
