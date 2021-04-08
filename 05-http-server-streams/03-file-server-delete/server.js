69ab30108bfd3f41097500db25b89f6da91ce151
Oleg Ledvanov <olegledvanov@gmail.com>
2021-04-08 13:04:01 +0300

    process HW #5 part 3

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,16 +1,32 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {stat, unlink} = require('fs/promises')
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (path.parse(filepath).dir != path.join(__dirname, 'files')){
+        res.statusCode = 400
+        res.end('Wrong path!')
+        return
+      }
+      try {
+        await stat(filepath)
+      } catch (err) {
+
+        await unlink(filepath)
+        res.statusCode = 201
+        res.end('OK')
+        return
+      }
 
       break;
 
